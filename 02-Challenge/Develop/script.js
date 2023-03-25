759918ded3423d2296be2df8d62ebb939c6e5d7b
Wesley Ashfield <lwashfield@gmail.com>
2023-03-25 11:22:38 -0400

    upd script

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -9,6 +9,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
