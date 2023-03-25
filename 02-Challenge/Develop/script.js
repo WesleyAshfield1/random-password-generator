10e702e9e167fc95ade68382624b551e36c3c5cc
Wesley Ashfield <lwashfield@gmail.com>
2023-03-25 11:42:14 -0400

    script upd

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,4 +1,24 @@
 // Assignment Code
+const includeUppercase = callback1.checked;
+const callback1 = document.getElementById('callback1')
+
+const includeLowercase = callback2.checked;
+const callback2 = document.getElementById('callback2')
+
+const includeNumber = callback3.checked;
+const callback3 = document.getElementById('callback3')
+
+const includeSpecialChar = callback4.checked;
+const callback4 = document.getElementById('callback4')
+
+//determine lenght of password
+var passwordLenght = document.getElementById('numberSelect')
+document.getElementById('numberSelect').addEventListener('change', function() {
+  passwordLength = this.value;
+});
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,7 +30,7 @@ function writePassword() {
 
 }
 function generatePassword() {
-  
+
 }
 
 // Add event listener to generate button
