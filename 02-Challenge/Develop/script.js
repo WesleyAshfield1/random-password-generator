bf217247c10f6008d4f4caa819e0354f23f4652c
Wesley Ashfield <lwashfield@gmail.com>
2023-03-25 12:41:47 -0400

    created functions to generate password

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,15 +1,16 @@
 // Assignment Code
-const includeUppercase = callback1.checked;
-const callback1 = document.getElementById('callback1')
+const cb1 = document.getElementById('cb1')
+var includeUppercase = cb1.checked;
 
-const includeLowercase = callback2.checked;
-const callback2 = document.getElementById('callback2')
+const cb2 = document.getElementById('cb2')
+const includeLowercase = cb2.checked;
 
-const includeNumber = callback3.checked;
-const callback3 = document.getElementById('callback3')
+const cb3 = document.getElementById('cb3')
+const includeNumber = cb3.checked;
 
-const includeSpecialChar = callback4.checked;
-const callback4 = document.getElementById('callback4')
+const cb4 = document.getElementById('cb4')
+const includeSpecialChar = cb4.checked;
+
 
 //determine lenght of password
 var passwordLenght = document.getElementById('numberSelect')
@@ -19,19 +20,35 @@ document.getElementById('numberSelect').addEventListener('change', function() {
 
 
 
-// var generateBtn = document.querySelector("#generate");
+ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword(passwordLenght, cb1.checked, cb2.checked, cb3.checked, cb4.checked);
+  const passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+   passwordText.value = password;
+    console.log (password)
+    //**password is blankn in the console log but shows activty when clicking gererate password **Debug
 }
-function generatePassword() {
+function generatePassword(lenght, includeUppercase, includeLowercase, includeNumber, includeSpecialChar) {
+  var allchars = "";
+  if (includeUppercase) allchars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (includeLowercase) allchars += "abcdefghijklmnopqrstuvwxyz";
+  if (includeNumber) allchars += "0123456789";
+  if (includeSpecialChar) allchars += "@#$%^&()_+~|}{[]></-=";
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    password += allchars.charAt(Math.floor(Math.random() * allchars.length));
+  }
+  return password;
 
+  console.log ('password')
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// console.log (writePassword)
+
